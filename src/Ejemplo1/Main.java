a771f2dbb92b3a33bfb1e42cb9be3aea249e8aca
Samuel Rubio <srubiom18@gmail.com>
2022-04-22 18:12:56 +0200

    Comienzo de proyecto

---
+++ src/Ejemplo1/Main.java
@@ -0,0 +1,17 @@
+package Ejemplo1;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num1, num2;
+		
+		System.out.print("Escribe un numero: "); num1 = sc.nextInt();
+		System.out.print("Escribe otro numero: "); num2 = sc.nextInt();
+		
+		System.out.println("El resultado es " + (num1 + num2));
+		
+ }
+}
